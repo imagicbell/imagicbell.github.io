251a8edf43499f83b3acbfd9b5dbe538f9206816
maoling <maoling@putao.com>
2021-03-06 14:58:46 +0800

    prune project

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,23 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
 
-export default function Index({ allPosts }) {
-  // const heroPost = allPosts[0]
-  const morePosts = allPosts;
+export default function Index({ }) {
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with</title>
-        </Head>
-        <Container>
-          <Intro />
-          {/* {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )} */}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
+    <Layout>
+      <Head>
+        <title>Next.js Blog Example with</title>
+      </Head>
+      <Container>
+
+      </Container>
+    </Layout>
   )
 }
 
 export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'categories'
-  ])
-  // console.log(allPosts)
   return {
-    props: { allPosts },
+    props: { },
   }
 }
