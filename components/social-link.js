01eca05a9c3dde6deda33ff281700ac15e596dc2
maoling <maoling@putao.com>
2021-03-15 23:28:04 +0800

    add github link

--- components/social-link.js
+++ components/social-link.js
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { Settings } from '../lib/constants';
 
 const links = [
 	{ icon: faEnvelopeSquare, href: `mailto:${Settings.author.email}`, color: "rgb(239, 68, 68)" },
 	{ icon: faLinkedin, href: `https://www.linkedin.com/in/${Settings.author.linkedin}`, color: "#007bb6"},
+	{ icon: faGithub, href: `https://github.com/${Settings.author.github}`, color: "#24292e"}
 ]
 
 export default function SocialLink() {
